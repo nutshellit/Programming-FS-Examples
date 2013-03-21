b8bf6b2f8b6697c94e040330d3ea522e35067783
Anthony Moloney <anthony.moloney@gmail.com>
2013-03-21 17:27:03 +0000

    Ch1 Fixtures

--- Scripts/MyTests/Ch1.fs
+++ Scripts/MyTests/Ch1.fs
@@ -1,10 +1,6 @@
 ﻿namespace MyTests.Tests
 
-open NUnit.Framework
-open FsUnit
-open System
-
-type Examples() =
+type Ch1Examples() =
     member this.Calc x = x + 1
     member this.PrintTruthTable f =
         printfn "       |true   | false |"
@@ -35,30 +31,3 @@ type Examples() =
                 yield x
                 yield x + 1
             ]
-
-[<TestFixture>]
-type ``Test From Ch1`` () =
-    let ex = new Examples()
-    let hardDriveSize = 250I * 1024I * 1024I * 1024I
-     
-    [<Test>] member test.
-     ``Basic`` () =
-        Int32.Parse "1" |> should equal 1
-
-    [<Test>] member test.
-     ``Basic test`` () =
-        ex.Calc 2 |> should equal 3
-
-    [<Test>] member test.
-     ``Truth table`` () =
-        ex.PrintTruthTable (&&) 
-
-    [<Test>] member test.
-     ``Zettabyte`` () =
-        ex.Zettabyte.ToString() |> should equal "1180591620717411303424"
-
-    [<Test>] member test.
-     ``bytesToGB`` () =
-        ex.bytesToGB hardDriveSize |> should equal 250I
-
-        
---
+++ Scripts/MyTests/Ch1Fixtures.fs
@@ -0,0 +1,34 @@
+﻿namespace MyTests.Tests
+
+open NUnit.Framework
+open FsUnit
+open System
+
+
+
+[<TestFixture>]
+type ``Test From Ch1`` () =
+    let ex = new Ch1Examples()
+    let hardDriveSize = 250I * 1024I * 1024I * 1024I
+     
+    [<Test>] member test.
+     ``Basic`` () =
+        Int32.Parse "1" |> should equal 1
+
+    [<Test>] member test.
+     ``Basic test`` () =
+        ex.Calc 2 |> should equal 3
+
+    [<Test>] member test.
+     ``Truth table`` () =
+        ex.PrintTruthTable (&&) 
+
+    [<Test>] member test.
+     ``Zettabyte`` () =
+        ex.Zettabyte.ToString() |> should equal "1180591620717411303424"
+
+    [<Test>] member test.
+     ``bytesToGB`` () =
+        ex.bytesToGB hardDriveSize |> should equal 250I
+
+        
