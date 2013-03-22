699f60321f4ee02b744c7245a9af3e5f9f1ae5dd
Anthony Moloney <anthony.moloney@gmail.com>
2013-03-22 09:09:53 +0000

    Complete ch1

--- Scripts/MyTests/Ch1.fs
+++ Scripts/MyTests/Ch1.fs
@@ -25,9 +25,65 @@ type Ch1Examples() =
         let x = x / 1024I
         printfn "Bytestogb %A" x
         x
+
     member this.numbersNear x =
             [   
                 yield x - 1
                 yield x
                 yield x + 1
             ]
+    member this.moreComplexListComprehension  =
+        [ 
+            let negate x = -x
+            for i in 1 .. 10 do
+                if i % 2 = 0 then 
+                    yield negate i
+                else
+                    yield i ]
+    member this.multiplesOf x = 
+        [ for i in 1 .. 10 do yield x * i ]
+
+    member this.multiplesOf2 x =
+        [ for i in 1 .. 10 -> x * i ]
+
+    member this.primesUnder max =
+        [   
+            for n in 1 .. max do
+            let factorsOfN =
+                [ 
+                    for i in 1 .. n do
+                        if n % i = 0 then
+                            yield i 
+                ]
+            
+            // n is prime if its only factors are 1 and n
+            if List.length factorsOfN = 2 then
+                yield n
+        ]
+    member this.ListPartitionMultiplesOf5 =
+        let isMultipleOf5 x = (x % 5 = 0)
+        let multOf5, nonMultOf5 = 
+            List.partition isMultipleOf5 [1 .. 15]
+        (multOf5, nonMultOf5)
+
+    member this.countVowels (str : string) =
+        let charList = List.ofSeq str
+
+        let accFunc(As, Es, Is, Os, Us) letter =
+            if letter = 'a' then (As + 1, Es, Is, Os, Us)
+            elif letter = 'e' then (As, Es + 1, Is, Os, Us)
+            elif letter = 'i' then (As, Es, Is + 1, Os, Us)
+            elif letter = 'o' then (As, Es, Is, Os + 1, Us)
+            elif letter = 'u' then (As, Es, Is, Os, Us + 1)
+            else                   (As, Es, Is, Os, Us)
+
+        List.fold accFunc (0,0,0,0,0) charList
+
+    member this.OptionNegativeNumbers intList =
+        let isLessThanZero x = (x < 0)
+        let filteredList = List.filter isLessThanZero intList
+        if List.length filteredList > 0
+        then Some(filteredList)
+        else None
+                    
+
--- Scripts/MyTests/Ch1Fixtures.fs
+++ Scripts/MyTests/Ch1Fixtures.fs
@@ -20,7 +20,7 @@ type ``Test From Ch1`` () =
         ex.Calc 2 |> should equal 3
 
     [<Test>] member test.
-     ``Truth table`` () =
+     ``Truth table boolean values`` () =
         ex.PrintTruthTable (&&) 
 
     [<Test>] member test.
@@ -30,5 +30,46 @@ type ``Test From Ch1`` () =
     [<Test>] member test.
      ``bytesToGB`` () =
         ex.bytesToGB hardDriveSize |> should equal 250I
+
+     [<Test>] member test.
+     ``numbersNear - list comprehension`` () =
+        ex.numbersNear 3 |> should equal [2;3;4]
+
+     [<Test>] member test.
+     ``more complex list comprehension`` () =
+        ex.moreComplexListComprehension |> should equal [1;-2;3;-4;5;-6;7;-8;9;-10]
+
+     [<Test>] member test.
+     ``generate first ten multiples of a number list comprehension`` () =
+         ex.multiplesOf 2 |> should equal [2;4;6;8;10;12;14;16;18;20]
+     
+     [<Test>] member test.
+     ``generate first ten multiples of a number list comprehension simplified`` () =
+         ex.multiplesOf2 3 |> should equal [3;6;9;12;15;18;21;24;27;30]
+
+    [<Test>] member test.
+     ``generate primes under 30`` () =
+        ex.primesUnder 30 |> should equal [2;3;5;7;11;13;17;19;23;29]
+
+    [<Test>] member test.
+     ``List partition - multiples of 5 from 1 to 15`` () =
+        ex.ListPartitionMultiplesOf5 |> should equal ([5;10;15], [1;2;3;4;6;7;8;9;11;12;13;14])
+
+     [<Test>] member test.
+     ``List aggregate - List fold count vowels (As, Es, Is, Os, Us)`` () =
+        ex.countVowels "The rain in spain falls mainly on the plain for you" 
+            |> should equal (5,2,5,3,1)
+
+     [<Test>] member test.
+     ``Option values - All positives`` () =
+        ex.OptionNegativeNumbers [1;2;3] |> should equal None
+
+      [<Test>] member test.
+      ``Option values - With negatives`` () =
+        ex.OptionNegativeNumbers [-3;1;-4;2;3]  |> should equal (Some [-3;-4] )
+
+
+
+
 
         
